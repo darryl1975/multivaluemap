f3c193b3f66736c971c6f1579bfe4f3cc4d79a9d
darryl1975 <darryl1975@hotmail.com>
2023-11-23 09:46:12 +0800

    completed multimapvalue demo

--- src/main/java/sg/edu/nus/iss/multivaluemap/controller/UserController.java
+++ src/main/java/sg/edu/nus/iss/multivaluemap/controller/UserController.java
@@ -1,8 +1,26 @@
 package sg.edu.nus.iss.multivaluemap.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
-    
+
+    @PostMapping("/user")
+    public String createUser(@RequestBody MultiValueMap<String, String> form, Model model) {
+        String name = form.getFirst("name");
+        String email = form.getFirst("email");
+        String phone = form.getFirst("phone");
+
+        System.out.println("name: " + name);
+        System.out.println("email: " + email);
+        System.out.println("phone: " + phone);
+
+        return "";
+    }
 }
